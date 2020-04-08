2310e3a6b967f29db1c6cec25c20a2ea7eb2e9fa
Micah McCallum <micahmccallum@outlook.com>
2020-04-08 01:52:02 +0000

    Automatically backed up by Learn

--- lib/vehicle.rb
+++ lib/vehicle.rb
@@ -1,7 +1,7 @@
 class Vehicle
   attr_accessor :wheel_size, :number
 
-  def initialize(wheel_size, number)
+  def initialize(wheel_size, wheel_number)
     @wheel_size = wheel_size
     @wheel_number = wheel_number
   end
